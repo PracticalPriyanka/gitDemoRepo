8216333298d1e05a1e0e81aecc2f748253293713
lnc <lnc@DIN76000547.corp.capgemini.com>
2016-10-19 16:07:28 +0530

    jdfhgjush

---
+++ gitRepo/src/main/java/gitRepo/MainClass.java
@@ -0,0 +1,10 @@
+package gitRepo;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		System.out.println("this is a main class");// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ gitRepo/src/main/java/gitRepo/MyName.java
@@ -0,0 +1,7 @@
+package gitRepo;
+
+public class MyName {
+	public void naming(){
+	System.out.println("priya");
+	}
+}
